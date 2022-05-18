e55ad229de95e6d8b918c41cf827a6b995b7a588
Hotklou <hotklou.ontwikkel@gmail.com>
2022-05-18 11:41:21 +0200

    Continued work on v3.0.

--- addons/ninepatchbutton_development/addons/ninepatchbutton/ninepatchbutton.gd
+++ addons/ninepatchbutton_development/addons/ninepatchbutton/ninepatchbutton.gd
@@ -9,32 +9,446 @@ signal on_button_up()
 signal on_mouse_entered()
 signal on_mouse_exited()
 
-
 export var is_disabled : bool = false
-export var enabled_normal : StyleBoxTexture
-export var enabled_hover : StyleBoxTexture
-export var enabled_focus : StyleBoxTexture
-export var enabled_pressed : StyleBoxTexture
+export (int, "Stereo", "Surround", "Center") var mix_target = 0
+export var bus_name = "Master"
 
+var default_parameters = {
+	# Enabled Textures
+	enabled_normal_texture = null,
+	enabled_hover_texture = null,
+	enabled_focus_texture = null,
+	enabled_pressed_texture = null,
+	# Disabled Textures
+	disabled_normal_texture = null,
+	disabled_hover_texture = null,
+	disabled_focus_texture = null,
+	disabled_pressed_texture = null,
+	# Enabled Sounds
+	enabled_hover_sound = null,
+	enabled_hover_volume_db = 0,
+	enabled_hover_pitch_scale = 1,
+	enabled_focus_sound = null,
+	enabled_focus_volume_db = 0,
+	enabled_focus_pitch_scale = 1,
+	enabled_pressed_sound = null,
+	enabled_pressed_volume_db = 0,
+	enabled_pressed_pitch_scale = 1,
+	enabled_pressed_hover_sound = null,
+	enabled_pressed_hover_volume_db = 0,
+	enabled_pressed_hover_pitch_scale = 1,
+	enabled_pressed_focus_sound = null,
+	enabled_pressed_focus_volume_db = 0,
+	enabled_pressed_focus_pitch_scale = 1,
+	# Disabled Sounds
+	disabled_hover_sound = null,
+	disabled_hover_volume_db = 0,
+	disabled_hover_pitch_scale = 1,
+	disabled_focus_sound = null,
+	disabled_focus_volume_db = 0,
+	disabled_focus_pitch_scale = 1,
+	disabled_pressed_sound = null,
+	disabled_pressed_volume_db = 0,
+	disabled_pressed_pitch_scale = 1,
+	disabled_pressed_hover_sound = null,
+	disabled_pressed_hover_volume_db = 0,
+	disabled_pressed_hover_pitch_scale = 1,
+	disabled_pressed_focus_sound = null,
+	disabled_pressed_focus_volume_db = 0,
+	disabled_pressed_focus_pitch_scale = 1,
+}
 
-export var disabled_normal : StyleBoxTexture
-export var disabled_hover : StyleBoxTexture
-export var disabled_focus : StyleBoxTexture
-export var disabled_pressed : StyleBoxTexture
-
-
-export var enabled_pressed_sound : AudioStream
-export var enabled_hover_sound : AudioStream
-export var enabled_focus_sound : AudioStream
-
-export var disabled_pressed_sound : AudioStream
-export var disabled_hover_sound : AudioStream
-export var disabled_focus_sound : AudioStream
+# Enabled Textures
+var enabled_normal_texture : StyleBoxTexture
+var enabled_hover_texture : StyleBoxTexture
+var enabled_focus_texture : StyleBoxTexture
+var enabled_pressed_texture : StyleBoxTexture
+# Disabled Textures
+var disabled_normal_texture : StyleBoxTexture
+var disabled_hover_texture : StyleBoxTexture
+var disabled_focus_texture : StyleBoxTexture
+var disabled_pressed_texture : StyleBoxTexture
+# Enabled Sounds
+var enabled_hover_sound : AudioStream
+var enabled_hover_volume_db : float
+var enabled_hover_pitch_scale : float
+var enabled_focus_sound : AudioStream
+var enabled_focus_volume_db : float
+var enabled_focus_pitch_scale : float
+var enabled_pressed_sound : AudioStream
+var enabled_pressed_volume_db : float
+var enabled_pressed_pitch_scale : float
+var enabled_pressed_hover_sound : AudioStream
+var enabled_pressed_hover_volume_db : float
+var enabled_pressed_hover_pitch_scale : float
+var enabled_pressed_focus_sound : AudioStream
+var enabled_pressed_focus_volume_db : float
+var enabled_pressed_focus_pitch_scale : float
+# Disabled Sounds
+var disabled_hover_sound : AudioStream
+var disabled_hover_volume_db : float
+var disabled_hover_pitch_scale : float
+var disabled_focus_sound : AudioStream
+var disabled_focus_volume_db : float
+var disabled_focus_pitch_scale : float
+var disabled_pressed_sound : AudioStream
+var disabled_pressed_volume_db : float
+var disabled_pressed_pitch_scale : float
+var disabled_pressed_hover_sound : AudioStream
+var disabled_pressed_hover_volume_db : float
+var disabled_pressed_hover_pitch_scale : float
+var disabled_pressed_focus_sound : AudioStream
+var disabled_pressed_focus_volume_db : float
+var disabled_pressed_focus_pitch_scale : float
 
 var audio
 
 
+# See https://github.com/Arnklit/ShellFurGodot/blob/main/addons/shell_fur/shell_fur_manager.gd
+# for examples on how to work with the `_get_property_list()` built-in function!
+func _get_property_list() -> Array:
+	var properties := Array()
 
+	properties.append({
+		name = "Enabled Textures",
+		type = TYPE_NIL,
+		hint_string = "enabled_",
+		usage = PROPERTY_USAGE_GROUP | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_normal_texture",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "StyleBoxTexture",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_hover_texture",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "StyleBoxTexture",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_focus_texture",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "StyleBoxTexture",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	properties.append({
+		name = "enabled_pressed_texture",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "StyleBoxTexture",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "Disabled Textures",
+		type = TYPE_NIL,
+		hint_string = "disabled_",
+		usage = PROPERTY_USAGE_GROUP | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_normal_texture",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "StyleBoxTexture",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_hover_texture",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "StyleBoxTexture",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_focus_texture",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "StyleBoxTexture",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	properties.append({
+		name = "disabled_pressed_texture",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "StyleBoxTexture",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+
+
+	properties.append({
+		name = "Enabled Sounds",
+		type = TYPE_NIL,
+		hint_string = "enabled_",
+		usage = PROPERTY_USAGE_GROUP | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_hover_sound",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "AudioStream",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_hover_volume_db",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "-80, 24",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_hover_pitch_scale",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "0.01, 4",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_focus_sound",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "AudioStream",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_focus_volume_db",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "-80, 24",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_focus_pitch_scale",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "0.01, 4",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_pressed_sound",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "AudioStream",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_pressed_volume_db",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "-80, 24",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_pressed_pitch_scale",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "0.01, 4",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_pressed_hover_sound",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "AudioStream",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_pressed_hover_volume_db",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "-80, 24",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_pressed_hover_pitch_scale",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "0.01, 4",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_pressed_focus_sound",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "AudioStream",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_pressed_focus_volume_db",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "-80, 24",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "enabled_pressed_focus_pitch_scale",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "0.01, 4",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+
+	properties.append({
+		name = "Disabled Sounds",
+		type = TYPE_NIL,
+		hint_string = "disabled_",
+		usage = PROPERTY_USAGE_GROUP | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_hover_sound",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "AudioStream",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_hover_volume_db",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "-80, 24",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_hover_pitch_scale",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "0.01, 4",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_focus_sound",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "AudioStream",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_focus_volume_db",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "-80, 24",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_focus_pitch_scale",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "0.01, 4",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_pressed_sound",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "AudioStream",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_pressed_volume_db",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "-80, 24",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_pressed_pitch_scale",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "0.01, 4",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_pressed_hover_sound",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "AudioStream",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_pressed_hover_volume_db",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "-80, 24",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_pressed_hover_pitch_scale",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "0.01, 4",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_pressed_focus_sound",
+		type = TYPE_OBJECT,
+		hint = PROPERTY_HINT_RESOURCE_TYPE,
+		hint_string = "AudioStream",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_pressed_focus_volume_db",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "-80, 24",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+	
+	properties.append({
+		name = "disabled_pressed_focus_pitch_scale",
+		type = TYPE_REAL,
+		hint = PROPERTY_HINT_RANGE,
+		hint_string = "0.01, 4",
+		usage = PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_SCRIPT_VARIABLE,
+	})
+
+	return properties
+
+
+func property_can_revert(property : String) -> bool:
+	if not default_parameters.has(property):
+		return false
+	if get(property) != default_parameters[property]:
+		return true
+	return false
 
 
 func _ready() -> void:
@@ -57,29 +471,30 @@ func _prepare_audioplayer() -> void:
 	self.add_child(node)
 	node.name = "AudioStreamPlayer"
 	audio = $AudioStreamPlayer
-	print(node,audio)
 	audio.autoplay = false
 
 
 func _prepare_styles() -> void:
-	match is_disabled:
-		false:
-			set("custom_styles/hover", enabled_hover)
-			set("custom_styles/pressed", enabled_pressed)
-			set("custom_styles/focus", enabled_focus)
-			set("custom_styles/normal", enabled_normal)
+	match disabled:
 		true:
-			set("custom_styles/hover", disabled_hover)
-			set("custom_styles/pressed", disabled_pressed)
-			set("custom_styles/focus", disabled_focus)
-			set("custom_styles/normal", disabled_normal)
+			set("custom_styles/disabled", disabled_normal_texture)
+		false:
+			match is_disabled:
+				false:
+					set("custom_styles/normal", enabled_normal_texture)
+					set("custom_styles/hover", enabled_hover_texture)
+					set("custom_styles/focus", enabled_focus_texture)
+					set("custom_styles/pressed", enabled_pressed_texture)
+				true:
+					set("custom_styles/normal", disabled_normal_texture)
+					set("custom_styles/hover", disabled_hover_texture)
+					set("custom_styles/focus", disabled_focus_texture)
+					set("custom_styles/pressed", disabled_pressed_texture)
 
-	#set("custom_styles/disabled", disabled_texture)
 
-
-func _prepare_signals(signal_name,object,method) -> void:
-	if not is_connected(signal_name,object,method):
-		var err = connect(signal_name,object,method)
+func _prepare_signals(signal_name, object, method) -> void:
+	if not is_connected(signal_name, object, method):
+		var err = connect(signal_name, object, method)
 		if err != OK:
 			print("Error can't connect signal on: ", str(object))
 
@@ -100,7 +515,7 @@ func _button_up() -> void:
 			pass
 
 
-func _mouse_entered():
+func _mouse_entered() -> void:
 	match is_disabled:
 		false:
 			play_sound(enabled_hover_sound)
@@ -109,12 +524,13 @@ func _mouse_entered():
 			play_sound(disabled_hover_sound)
 
 
-func _mouse_exited():
+func _mouse_exited() -> void:
 	match is_disabled:
 		false:
 			emit_signal("on_mouse_exited")
 		true:
 			pass
+
 
 func _pressed() -> void:
 	match is_disabled:
@@ -124,7 +540,6 @@ func _pressed() -> void:
 		true:
 			play_sound(disabled_pressed_sound)
 			pressed = false
-
 
 
 func _toggled(button_pressed: bool) -> void:
